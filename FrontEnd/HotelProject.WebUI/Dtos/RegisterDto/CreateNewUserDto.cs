0ff96c1ac2821216e4a3b00a8f05e3d068699173
Ahmet Bogar <ahmetbogar@hotmail.com>
2023-09-03 04:42:44 +0300

    03.09.23

---
+++ FrontEnd/HotelProject.WebUI/Dtos/RegisterDto/CreateNewUserDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.RegisterDto
+{
+    public class CreateNewUserDto
+    {
+        [Required(ErrorMessage ="Ad Giriniz")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Soyad Giriniz")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Kullanıcı Adı Giriniz")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Mail Giriniz")]
+        public string Mail { get; set; }
+
+        [Required(ErrorMessage = "Şifre Giriniz")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Şifrenizi Tekrar Giriniz")]
+        [Compare("Password",ErrorMessage ="Şifreler Uyuşmuyor")]
+        public string ConfirmPassword { get; set; }
+    }
+}
